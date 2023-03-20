5e64142aa6446dce330f0654431b13d263975788
Elvis Okereke <57872669+ElvisOkereke@users.noreply.github.com>
2023-03-20 15:58:46 -0400

    fixing bad link

--- app/page.tsx
+++ app/page.tsx
@@ -195,11 +195,7 @@ export default function Home() {
             <div className="sm:flex sm:items-center sm:justify-between">
               <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
                 © 2022{" "}
-                <a
-                  href="https://flowbite.com"
-                  target="_blank"
-                  className="hover:underline"
-                >
+                <a href="" target="_blank" className="hover:underline">
                   LVBT™
                 </a>
                 . All Rights Reserved.
