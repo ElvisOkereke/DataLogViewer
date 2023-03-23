dd72832168fcd4bf03cce6800a803f8dba2f81e7
Elvis Okereke <57872669+ElvisOkereke@users.noreply.github.com>
2023-03-22 20:05:29 -0400

    dummy data available

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { BsGrid } from "react-icons/bs";
 import { CiGrid2H } from "react-icons/ci";
 import { ImCheckboxUnchecked } from "react-icons/im";
+import { AiOutlineDownload } from "react-icons/ai";
 import {
   LineChart,
   Line,
@@ -19,12 +20,9 @@ import {
   ResponsiveContainer,
 } from "recharts";
 import { FullWidth } from "ag-grid-community/dist/lib/components/framework/componentTypes";
+import Link from "next/link";
 
 type Props = {};
-
-//const windowSize = useRef([window.innerWidth, window.innerHeight]);
-//Width: {windowSize.current[0]}
-//Height: {windowSize.current[1]}
 
 const productList = (data: any) => {
   for (let i = 0; i < data.length; i++) {
@@ -39,56 +37,59 @@ const productList = (data: any) => {
   }
 };
 
-const resize = () => {};
-
-const drawChart = (fields: any) => {
-  //for (let i = 0; i < fields.length; i++) {
-  // console.log(fields[i].trim().toUpperCase().replaceAll(" ", "_"));
-  // return<Line type="monotone" dataKey={fields[i].trim().toUpperCase().replaceAll(" ", "_")} stroke="#ffc658"/>;
-  // } DOESNT WORK BECAUSE OF THE RETURN STOPS THE LOOP
-
-  let colors = [
-    "#ffc658",
-    "#82ca9d",
-    "#8884d8",
-    "#ff0000",
-    "#00ff00",
-    "#e6194b",
-    "#3cb44b",
-    "#ffe119",
-    "#4363d8",
-    "#f58231",
-    "#911eb4",
-    "#46f0f0",
-    "#f032e6",
-    "#bcf60c",
-    "#fabebe",
-    "#008080",
-    "#e6beff",
-    "#9a6324",
-    "#800000",
-    "#aaffc3",
-    "#808000",
-    "#ffd8b1",
-    "#000075",
-  ];
-
-  return fields.map((field: string, id: number) => {
-    return (
-      <Line
-        activeDot={{ r: 4 }}
-        type="linear"
-        dataKey={field.trim().toUpperCase().replaceAll(" ", "_")}
-        stroke={colors[id]}
-        strokeWidth={3}
-      />
-    );
-  });
-};
-
 function page({}: Props) {
   const [data, setData] = useState<any>([]);
   const [fields, setFields] = useState<any>([]);
+  let max: any, maxId: any;
+
+  const drawChart = (fields: any) => {
+    //for (let i = 0; i < fields.length; i++) {
+    //max == null ? (max = 0) : max;
+    //if (Math.max(datatmp[i]) > max) {
+    //max = Math.max(datatmp[i]);
+    // maxId = i;
+    //  }
+    // }
+
+    let colors = [
+      "#ffc658",
+      "#82ca9d",
+      "#8884d8",
+      "#ff0000",
+      "#00ff00",
+      "#e6194b",
+      "#3cb44b",
+      "#ffe119",
+      "#4363d8",
+      "#f58231",
+      "#911eb4",
+      "#46f0f0",
+      "#f032e6",
+      "#bcf60c",
+      "#fabebe",
+      "#008080",
+      "#e6beff",
+      "#9a6324",
+      "#800000",
+      "#aaffc3",
+      "#808000",
+      "#ffd8b1",
+      "#000075",
+    ];
+
+    return fields.map((field: string, id: number) => {
+      return (
+        <Line
+          key={id}
+          activeDot={{ r: 4 }}
+          type="linear"
+          dataKey={field.trim().toUpperCase().replaceAll(" ", "_")}
+          stroke={colors[id]}
+          strokeWidth={3}
+        />
+      );
+    });
+  };
 
   const handleFileUpload = (event: any) => {
     const file = event.target.files[0];
@@ -166,6 +167,20 @@ function page({}: Props) {
               <BsGrid className=" h-10 w-10 mt-2 mr-2" />
               <div className="text-xl font-mono font-bold pt-4">Quad Chart</div>
             </button>
+            <Link href="https://cdn.discordapp.com/attachments/566802843642888192/1088235509769310258/dummy_autodata_-_Sheet2.csv">
+              <button
+                type="button"
+                className="inline-flex text-white bg-gradient-to-r
+             from-[#f58231] to-[#F7AB0A] hover:bg-gradient-to-l focus:outline-none
+              focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-2xl
+               text-sm px-5 py-2.5 text-center mr-2"
+              >
+                <AiOutlineDownload className=" h-10 w-10 mt-2 mr-2" />
+                <div className="text-xl font-mono font-bold pt-4">
+                  Dummy data
+                </div>
+              </button>
+            </Link>
           </div>
         </form>
       </section>
@@ -174,7 +189,7 @@ function page({}: Props) {
         id="main"
         className="snap-center col-span-12 rounded-lg border border-gray-500 p-32"
       >
-        <div className="w-[2000px] h-[1100px]">
+        <div className="w-full h-full">
           <ResponsiveContainer>
             <LineChart
               width={1500}
@@ -187,9 +202,9 @@ function page({}: Props) {
                 bottom: 5,
               }}
             >
-              <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey={fields[0]} />
-              <YAxis />
+
+              <YAxis domain={["auto", Math.max(data[maxId])]} />
               <Tooltip />
               <Legend />
               {drawChart(fields)}
